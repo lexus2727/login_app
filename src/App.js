037e70f47ecac23c1503ac995b47bc8b955c0d6f
lexus2727 <lexus2424@yahoo.com>
2020-04-26 23:47:29 -0400

    setting up async functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,39 @@ import SubmitButton  from './SubmitButton';
 import './App.css';
 
 class App extends React.Component {
+
+  async componentDidMount() {
+
+     try {
+
+        let res = await fetch('/isLoggedIn', {
+            method: 'post',
+            headers: {
+              'Accept': 'application/json',
+              'Content-type': 'application/json'
+            }
+        })
+
+      let result = await res.json();
+
+      if (result&&result.success) {
+        UserStore.loading = false;
+        UserStore.isLoggedIn = true;
+        UserStore.username = result.username;
+      }
+
+      else {
+        UserStore.loading = false;
+        UserStore.isLoggedIn = false;
+      }
+
+     }
+     catch(e) {
+       UserStore.loading = false;
+       UserStore.isLoggedIn = false;
+
+     }
+  }
   render() {
  return (
      <div className="app">
