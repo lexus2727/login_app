2e5671a04241fa93ca2d02160e1d2988ba90231d
lexus2727 <lexus2424@yahoo.com>
2020-04-29 23:04:31 -0400

    update frontend

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -30,10 +30,63 @@ class LoginForm extends React.Component {
             buttonDisabled: false
         })
     }
+
+    async doLogin() {
+        if (!this.state.username) {
+            return;
+        }
+
+        if (!this.state.password) {
+            return;
+        }
+        this.setState({
+            buttonDisabled: true
+        })
+        try {
+            let res = await fetch('/login', {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                   username: this.state.username,
+                   password: this.state.password
+                })
+            })
+
+            let result = await res.json();
+            if (result && result.success) {
+                UserStore.isLoggedIn = true;
+                UserStore.username = result.username;
+            }
+            else if (result && result.success === false) {
+                this.resetForm();
+                alert(result.msg);
+            }
+
+        }
+        catch(e) {
+
+            console.log(e);
+            this.resetForm();
+
+        }
+    }
+
+    
+
+    
   render() {
  return (
      <div className="loginForm">
-        Login Form
+        Log In
+        <InputField
+            type="text"
+            placeholder="username"
+            value={this.state.username ? this.state.username: ''}
+            onChange= { (val) => this.setInputValue('username', val) }
+            />
      </div>
  )
   }
